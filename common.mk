ab6911e29ffcb3da979b48e2e5bb4d5f6a9fb538
Evisceration <eviscerationls@gmail.com>
2014-02-08 16:12:27 +0100

    vendor: proprietary: switch to SuperSU from Chainfire
    Change-Id: Ie68b69c57073a3d0fc7b5cc064ecb8b6ff8eb816

--- common.mk
+++ common.mk
@@ -1,1 +1,8 @@
-
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/nameless/proprietary/prebuilt/bin/su:system/bin/.ext/.su \
+    vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/daemonsu \
+    vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/su \
+    vendor/nameless/proprietary/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+    vendor/nameless/proprietary/prebuilt/etc/installed_su_daemon:/system/etc/.installed_su_daemon \
+    vendor/nameless/proprietary/prebuilt/priv-app/Superuser.apk:system/priv-app/Superuser.apk
