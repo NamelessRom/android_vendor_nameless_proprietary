00a5b68096353449a59487370400d7e503daba3e
jumoog <kilianvonpflugk@gmail.com>
2014-05-03 21:41:08 +0200

    Track Playstore Dashclock
    Change-Id: I75f8f589274e76395d18dc157b1a4863cd0bb6d5

--- common.mk
+++ common.mk
@@ -6,3 +6,7 @@ PRODUCT_COPY_FILES += \
     vendor/nameless/proprietary/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
     vendor/nameless/proprietary/prebuilt/etc/installed_su_daemon:/system/etc/.installed_su_daemon \
     vendor/nameless/proprietary/prebuilt/app/Superuser.apk:system/app/Superuser.apk
+
+# Dashclock
+PRODUCT_COPY_FILES += \
+    vendor/nameless/proprietary/prebuilt/app/Dashclock.apk:system/app/Dashclock.apk
