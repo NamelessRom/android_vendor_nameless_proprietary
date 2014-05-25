39865f41a111205554595795a9d17b64df582c7c
Evisceration <eviscerationls@gmail.com>
2014-05-25 22:40:04 +0200

    Update SuperSU to v1.99
    Change-Id: I75f367d2006ef6af7633faf3d5b40312d261379e

--- common.mk
+++ common.mk
@@ -3,6 +3,7 @@ PRODUCT_COPY_FILES += \
     vendor/nameless/proprietary/prebuilt/bin/su:system/bin/.ext/.su \
     vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/daemonsu \
     vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/su \
+    vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/sugote \
     vendor/nameless/proprietary/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
     vendor/nameless/proprietary/prebuilt/etc/installed_su_daemon:/system/etc/.installed_su_daemon \
     vendor/nameless/proprietary/prebuilt/app/Superuser.apk:system/app/Superuser.apk
