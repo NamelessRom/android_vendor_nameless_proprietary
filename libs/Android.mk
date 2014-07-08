41f7d4110bd56e354fc895adcf7fa834dbe1e695
Evisceration <eviscerationls@gmail.com>
2014-07-08 11:10:18 +0200

    libs: remove butterknife
    Change-Id: Ic79f8ed07007a9cbbda0728acd23b5f556578567

--- libs/Android.mk
+++ libs/Android.mk
@@ -1,28 +1,19 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := butterknife-5.0.1.jar
-LOCAL_MODULE := butterknife
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
-
-include $(BUILD_PREBUILT)
-
+#===================================================================
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := crouton-1.8.4.jar
 LOCAL_MODULE := crouton
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
-
 include $(BUILD_PREBUILT)
-
+#===================================================================
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dashclock-api-r2.0.jar
 LOCAL_MODULE := dashclockapi
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
-
 include $(BUILD_PREBUILT)
+#===================================================================
