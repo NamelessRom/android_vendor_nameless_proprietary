2e1a1cdcba1d8d7b3f86c207426706c17dbf6719
Evisceration <eviscerationls@gmail.com>
2014-07-12 19:42:15 +0200

    libs: add support v13, revision 20 prebuilt
    Change-Id: Ie14fc23efc6886fcd5e25b16e12b06c9a9eb1b2d

--- libs/Android.mk
+++ libs/Android.mk
@@ -2,6 +2,14 @@ LOCAL_PATH:= $(call my-dir)
 
 #===================================================================
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := android-support-v13-r20.jar
+LOCAL_MODULE := android-support-v13-r20
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
+include $(BUILD_PREBUILT)
+#===================================================================
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := crouton-1.8.4.jar
 LOCAL_MODULE := crouton
 LOCAL_MODULE_TAGS := optional
