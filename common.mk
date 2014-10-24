cc60de5ce01adfa4cf5a63b3526c12782fa7930a
Evisceration <eviscerationls@gmail.com>
2014-10-24 09:51:35 +0200

    drop prebuilt supersu, add supersu zip for flashing
    Change-Id: I913573cd0bbc0de7a0bda8b592a7b7320613d77f

--- common.mk
+++ common.mk
@@ -1,9 +1,3 @@
 # SuperSU
 PRODUCT_COPY_FILES += \
-    vendor/nameless/proprietary/prebuilt/bin/su:system/bin/.ext/.su \
-    vendor/nameless/proprietary/prebuilt/bin/supolicy:/system/xbin/supolicy \
-    vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/daemonsu \
-    vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/su \
-    vendor/nameless/proprietary/prebuilt/bin/su:system/xbin/sugote \
-    vendor/nameless/proprietary/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
-    vendor/nameless/proprietary/prebuilt/etc/installed_su_daemon:/system/etc/.installed_su_daemon
+    vendor/nameless/proprietary/zips/supersu.zip:system/supersu.zip
